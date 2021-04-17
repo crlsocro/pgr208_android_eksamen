ef235bcb2285fd70761423887212b874467a1810
haako <haakonhagen@live.no>
2021-04-17 07:50:16 +0200

    Bug hunting

--- app/src/main/java/com/example/crypto/database/CryptoDatabase.kt
+++ app/src/main/java/com/example/crypto/database/CryptoDatabase.kt
@@ -3,7 +3,7 @@ package com.example.crypto.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(version = 1, entities = [Transaction::class, Balance::class])
+@Database(version = 1, entities = arrayOf(Balance::class, Transaction::class))
 abstract class CryptoDatabase : RoomDatabase(){
 
     abstract fun balanceDAO(): BalanceDAO
