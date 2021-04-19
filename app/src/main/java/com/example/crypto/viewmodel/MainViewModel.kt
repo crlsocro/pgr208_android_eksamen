0fd8e7ff36402b8fd298df816282d26a4ef3b6f2
haako <haakonhagen@live.no>
2021-04-19 17:32:30 +0200

    fixed and broken

---
+++ app/src/main/java/com/example/crypto/viewmodel/MainViewModel.kt
@@ -0,0 +1,46 @@
+package com.example.crypto.viewmodel
+
+import android.content.Context
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.crypto.database.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class MainViewModel(context: Context) : ViewModel() {
+
+    private var balanceDao : BalanceDAO = CryptoDatabase.get(context).getBDAO()
+    private var liveBalance = MutableLiveData<List<Balance>>()
+
+    fun getBalances() : MutableLiveData<List<Balance>> {
+
+        viewModelScope.launch {
+            var resultB = withContext(Dispatchers.IO){
+                balanceDao.getAllBalances()
+            }
+
+            liveBalance.value = resultB // updating the value
+        }
+
+        return liveBalance
+    }
+
+    private var transactionDao : TransactionDAO = CryptoDatabase.get(context).getTDAO()
+    private var liveTransaction = MutableLiveData<List<Transaction>>()
+
+    fun getTransactions() : MutableLiveData<List<Transaction>> {
+
+        viewModelScope.launch {
+            var resultT = withContext(Dispatchers.IO){
+                transactionDao.getAllTransactions()
+            }
+
+            liveTransaction.value = resultT // updating the value
+        }
+
+        return liveTransaction
+    }
+
+}
