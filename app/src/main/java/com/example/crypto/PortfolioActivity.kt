21f0c9f2c6f06fcdd92f9e75c251edb23da216d5
haako <haakonhagen@live.no>
2021-04-20 09:00:01 +0200

    removed database from the main thread

--- app/src/main/java/com/example/crypto/PortfolioActivity.kt
+++ app/src/main/java/com/example/crypto/PortfolioActivity.kt
@@ -9,6 +9,7 @@ import com.example.crypto.database.Balance
 import com.example.crypto.database.CryptoDatabase
 import com.example.crypto.database.Transaction
 import com.example.crypto.viewmodel.BalanceViewModel
+import com.example.crypto.viewmodel.MainViewModel
 import com.example.crypto.viewmodel.TransactionViewModel
 
 //Screen 3
@@ -16,6 +17,7 @@ class PortfolioActivity : AppCompatActivity() {
 
     private lateinit var viewModelB: BalanceViewModel
     private lateinit var viewModelT: TransactionViewModel
+    private lateinit var viewModel : MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +25,24 @@ class PortfolioActivity : AppCompatActivity() {
 
         viewModelB = BalanceViewModel(this)
         viewModelT = TransactionViewModel(this)
+        viewModel = MainViewModel(this)
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        viewModel.getBalances().observe(this, { list->
+            list.map {entity->
+                Log.d(this.javaClass.simpleName, entity.toString())
+            }
+        })
+
+        viewModel.getTransactions().observe(this, { list->
+            list.map {entity->
+                Log.d(this.javaClass.simpleName, entity.toString())
+            }
+        })
 
     }
 
@@ -31,17 +51,23 @@ class PortfolioActivity : AppCompatActivity() {
         viewModelB.addCryptoBalance(testB)
 
         //Display the database (test)
-        CryptoDatabase.get(this).getBDAO().getAllBalances().map { testB ->
+        /*CryptoDatabase.get(this).getBDAO().getAllBalances().map { testB ->
             Log.d(this.javaClass.simpleName, testB.toString())
-        }
+        }*/
 
         var testT = Transaction(0, "btc", 01.000, 50000.000, "bought")
         viewModelT.addCryptoTransaction(testT)
 
         //Display the database (test)
-        CryptoDatabase.get(this).getTDAO().getAllTransactions().map { testT ->
+        /*CryptoDatabase.get(this).getTDAO().getAllTransactions().map { testT ->
             Log.d(this.javaClass.simpleName, testT.toString())
-        }
+        }*/
+
+        viewModel.getTransactions().observe(this, { list->
+            list.map {entity->
+                Log.d(this.javaClass.simpleName, entity.toString())
+            }
+        })
 
     }
 
