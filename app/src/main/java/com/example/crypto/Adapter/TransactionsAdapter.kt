858dd895389201c4bc3e3f54afaff8e17bee47d4
haako <haakonhagen@live.no>
2021-04-21 17:58:51 +0200

    Id names and picasso

---
+++ app/src/main/java/com/example/crypto/Adapter/TransactionsAdapter.kt
@@ -0,0 +1,13 @@
+package com.example.crypto.Adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.example.crypto.databinding.ItemCryptoViewBinding
+
+class TransactionsAdapter {
+
+        /*override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TransactionsAdapter.CryptoViewHolder {
+        val binding = ItemCryptoViewBinding.inflate(LayoutInflater.from(parent.context))
+        return CryptoListAdapter.CryptoViewHolder(binding)
+    }*/
+}
