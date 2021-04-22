12028ba80e16df56abd649793f0a9b1e8391bae7
haako <haakonhagen@live.no>
2021-04-22 20:46:47 +0200

    Sending extras, not working

--- app/src/main/java/com/example/crypto/Adapter/CryptoListAdapter.kt
+++ app/src/main/java/com/example/crypto/Adapter/CryptoListAdapter.kt
@@ -1,9 +1,11 @@
 package com.example.crypto.Adapter
 
 import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat.startActivity
+import androidx.core.net.toUri
 import androidx.recyclerview.widget.RecyclerView
 import com.example.crypto.BuySellActivity
 import com.example.crypto.PortfolioActivity
@@ -11,6 +13,8 @@ import com.example.crypto.R
 import com.example.crypto.Repo.CryptoStats
 import com.example.crypto.databinding.ItemCryptoViewBinding
 import com.squareup.picasso.Picasso
+import java.net.URI
+import java.net.URL
 import kotlin.math.roundToInt
 import kotlin.math.roundToLong
 
@@ -31,9 +35,7 @@ class CryptoListAdapter(private var list: List<CryptoStats>) : RecyclerView.Adap
 
     class CryptoViewHolder(private val binding: ItemCryptoViewBinding) : RecyclerView.ViewHolder(binding.root)
     {
-        init {
 
-        }
         fun bind(stats: CryptoStats) {
             Picasso.get().load("https://static.coincap.io/assets/icons/${stats.symbol?.toLowerCase()}@2x.png").into(binding.imageViewIcon)
             binding.textViewCryptoPriceUsd.text  ="$" + "%.3f".format(stats.priceUsd?.toDouble())
@@ -43,12 +45,20 @@ class CryptoListAdapter(private var list: List<CryptoStats>) : RecyclerView.Adap
             binding.root.setOnClickListener{
                 println("test")
                 val intent = Intent(binding.root.context, BuySellActivity::class.java).apply {}
-                intent.putExtra(Intent.EXTRA_TEXT, "${stats.symbol}")
-                intent.putExtra(Intent.EXTRA_TEXT, "${stats.priceUsd}.toDouble()")
-                intent.putExtra(Intent.EXTRA_TEXT, "${stats.name}")
+                //intent.putExtra(Intent.EXTRA_TEXT, "${stats.symbol}")
+                //intent.putExtra(Intent.EXTRA_TEXT, "${stats.priceUsd}.toDouble()")
+                intent.putExtra("Name", "${stats.name}")
                 binding.root.context.startActivity(intent)
+
+
             }
         }
+
+        fun sendExtras(stats: CryptoStats) {
+            //TODO set extras her som sett i https://developer.android.com/training/sharing/send rundt send multiple blablabla
+
+        }
+
     }
 
     fun update(newList: List<CryptoStats>){
--- app/src/main/java/com/example/crypto/BuySellActivity.kt
+++ app/src/main/java/com/example/crypto/BuySellActivity.kt
@@ -2,6 +2,8 @@ package com.example.crypto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.crypto.Adapter.CryptoListAdapter
+import java.util.jar.Attributes
 
 //Screen 4
 
@@ -15,7 +17,8 @@ class BuySellActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_buy_sell)
 
-        val i : String = intent.extras.toString()
+        val i : String? = intent.extras?.getString(Name, "btc")
+        val j : String? = getIntent().getExtras()?.getString(CryptoListAdapter, "btc")
         println(i)
     }
 
