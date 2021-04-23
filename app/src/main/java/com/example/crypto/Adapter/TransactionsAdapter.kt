6673c581d2aee6c891ba5fb52425eba1bb7ae8da
hkonn <hkonnhaagaas@gmail.com>
2021-04-23 18:14:17 +0200

    made transactionadapter

--- app/src/main/java/com/example/crypto/Adapter/TransactionsAdapter.kt
+++ app/src/main/java/com/example/crypto/Adapter/TransactionsAdapter.kt
@@ -1,33 +1,47 @@
 package com.example.crypto.Adapter
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.crypto.BuySellActivity
 import com.example.crypto.Repo.CryptoStats
+import com.example.crypto.database.Balance
 import com.example.crypto.databinding.ItemCryptoViewBinding
 import com.example.crypto.databinding.ItemTransactionViewBinding
 import com.squareup.picasso.Picasso
 
-/*
+class TransactionsAdapter(val lambdaFunction: (Balance) -> Unit) : RecyclerView.Adapter<TransactionsAdapter.ViewHolder>(){
 
-class TransactionsAdapter() : RecyclerView.Adapter<CryptoListAdapter.CryptoViewHolder>(){
+    private val balanceList = mutableListOf<Balance>()
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TransactionsAdapter.TransactionsViewHolder {
-        val binding = ItemTransactionViewBinding.inflate(LayoutInflater.from(parent.context))
-        return TransactionsAdapter.TransactionsViewHolder(binding)
-    }
-
-    class TransactionsViewHolder(private val binding: ItemTransactionViewBinding) : RecyclerView.ViewHolder(binding.root)
-    {
-        fun bind(stats: CryptoStats) {
-            Picasso.get().load("https://static.coincap.io/assets/icons/${stats.symbol?.toLowerCase()}@2x.png").into(binding.imageViewIconTransaction)
-            binding.textViewCryptoName.text  = "${stats.name}"
-            binding.textViewCryptoSymbol.text = "${stats.symbol}"
-            binding.textViewCryptoPriceUsd.text  = "${stats.priceUsd}"
-            binding.textViewPercent.text = "${stats.changePercent24Hr}%"
-
+    class ViewHolder(val binding: ItemTransactionViewBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(balance: Balance) {
+            binding.textViewPrice.text = balance.balanceID.toString()
+            binding.textViewCryptoNameTransaction.text = balance.currency
         }
     }
-}
 
-*/
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val holder = ViewHolder(ItemTransactionViewBinding.inflate(LayoutInflater.from(parent.context)))
+        holder.itemView.setOnClickListener {
+            lambdaFunction(balanceList[holder.adapterPosition])
+        }
+        return holder
+    }
+
+    override fun getItemCount(): Int {
+        return balanceList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(balanceList[position])
+    }
+
+    fun setBalanceList(list: List<Balance>) {
+        balanceList.clear()
+        balanceList.addAll(list)
+        notifyDataSetChanged()
+    }
+
+}
