8a6d2483cee39f470f500ac0197822e63fb6e700
hkonn <hkonnhaagaas@gmail.com>
2021-04-24 21:51:10 +0200

    fixed buy

--- app/src/main/java/com/example/crypto/BuyActivity.kt
+++ app/src/main/java/com/example/crypto/BuyActivity.kt
@@ -8,16 +8,23 @@ import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.crypto.database.Balance
+import com.example.crypto.database.Transaction
+import com.example.crypto.viewmodel.BalanceViewModel
+import com.example.crypto.viewmodel.TransactionViewModel
 import com.squareup.picasso.Picasso
 
 
 //Screen 5
 
 class BuyActivity : AppCompatActivity() {
+
+    private lateinit var viewModelT: TransactionViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(com.example.crypto.R.layout.activity_buy)
-
+/*
         var btn : Button = findViewById(com.example.crypto.R.id.buyButton)
         btn.isEnabled = false
 
@@ -25,7 +32,7 @@ class BuyActivity : AppCompatActivity() {
         if (Integer.parseInt(usdEdit.text.toString()) > 5 || usdEdit.text.toString() == ""){
             btn.isEnabled = true
         }
-
+*/
         var intent = intent
         val symbol = intent.getStringExtra("symbol")
         val price = intent.getStringExtra("price")
@@ -38,7 +45,14 @@ class BuyActivity : AppCompatActivity() {
         textViewPrice.text = price
         textViewName.text = name
         Picasso.get().load("https://static.coincap.io/assets/icons/${symbol?.toLowerCase()}@2x.png").into(imageView)
+        viewModelT = TransactionViewModel(this)
+
+        var testT = Transaction(0, "btc", 01.000, 50000.000, "bought")
+        viewModelT.addCryptoTransaction(testT)
+
     }
+
+
 
 }
 
