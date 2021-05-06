86255e82b215b8209e6392758f1db7b765bc5152
chris <christian.r.lie@hotmail.no>
2021-05-06 15:49:07 +0200

    biiiggg push

--- app/src/main/java/com/example/crypto/database/CryptoDatabase.kt
+++ app/src/main/java/com/example/crypto/database/CryptoDatabase.kt
@@ -7,7 +7,7 @@ import androidx.room.RoomDatabase
 
 
 
-@Database(entities = [Balance::class, Transaction::class], version = 2)
+@Database(entities = [Balance::class, Transaction::class], version = 3)
 abstract class CryptoDatabase : RoomDatabase(){
     abstract fun getBDAO(): BalanceDAO
     abstract fun getTDAO(): TransactionDAO
